f9a7bb841a94b34532a04ca6366f0d4fb6f82735
Avinash Niyas <avinashNiyaz1423@gmail.com>
2025-11-09 17:50:34 +0530

    chnanged CORS origin for nginx

--- backEnd/demo/src/main/java/com/example/demo/NumberController.java
+++ backEnd/demo/src/main/java/com/example/demo/NumberController.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.*;
 
 
 @Controller
-@CrossOrigin(origins = "http://13.60.37.124:5173")
+@CrossOrigin(origins = "http://13.60.37.124")
 public class NumberController {
 
 
